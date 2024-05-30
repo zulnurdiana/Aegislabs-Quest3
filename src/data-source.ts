b3211567d20a84b34cc187e113e4e686dd156031
Zulfikar Nurdiana <66067543+zulnurdiana@users.noreply.github.com>
2024-05-30 17:38:20 +0700

    refine: datasource

--- src/data-source.ts
+++ src/data-source.ts
@@ -10,7 +10,6 @@ import { Cart } from "./entity/Cart"
 export const AppDataSource = new DataSource({
     type: "mongodb",
     database: "quest3",
-    url: "mongodb://zulnurdiana:0GCkJ5UJPfuA4N8A@ac-dr9rqg0-shard-00-00.w67zqzg.mongodb.net:27017,ac-dr9rqg0-shard-00-01.w67zqzg.mongodb.net:27017,ac-dr9rqg0-shard-00-02.w67zqzg.mongodb.net:27017/?ssl=true&replicaSet=atlas-ial56o-shard-0&authSource=admin&retryWrites=true&w=majority",
     synchronize: true,
     logging: false,
     ssl: true,
